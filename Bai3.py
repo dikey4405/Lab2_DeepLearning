dd822ece0a6fec46ca6699def08098ea9bcb2dac
dikey4405 <167569079+dikey4405@users.noreply.github.com>
2025-10-30 23:35:14 +0700

    Update Bai3.py

--- Bai3.py
+++ Bai3.py
@@ -5,6 +5,7 @@ from torch import nn, optim
 import numpy as np
 from sklearn.metrics import precision_score, recall_score, f1_score
 from model.RestNet import ResNet, ResidualBlock
+import os
 
 def evaluate(dataloader: DataLoader, model = nn.Module) -> dict:
     model.eval()
@@ -24,10 +25,35 @@ def evaluate(dataloader: DataLoader, model = nn.Module) -> dict:
         "f1": f1_score(trues, predictions, average="macro", zero_division=0)
         }
 
+def compute_scores(y_true, y_pred) -> dict:
+    return {
+        "accuracy": accuracy_score(y_true, y_pred),
+        "precision": precision_score(y_true, y_pred, average="macro", zero_division=0),
+        "recall": recall_score(y_true, y_pred, average="macro", zero_division=0),
+        "f1": f1_score(y_true, y_pred, average="macro", zero_division=0)
+    }
+
+def evaluate_per_class(preds, labels, num_classes):
+    for cls in range(num_classes):
+        class_preds = [1 if p == cls else 0 for p in preds]
+        class_labels = [1 if l == cls else 0 for l in labels]
+
+        print(f"\n----- Evaluation Results for Class {cls} -----")
+        scores = compute_scores(class_labels, class_preds)
+        print(f"Accuracy: {scores['accuracy']:.4f}")
+        print(f"Precision: {scores['precision']:.4f}")
+        print(f"Recall: {scores['recall']:.4f}")
+        print(f"F1_Score: {scores['f1']:.4f}")
+
 if __name__ == "__main__":
 
-    train_dataset = VinaFood("/kaggle/input/dataset-for-lab2/VinaFood21/train")
-    test_dataset  = VinaFood("/kaggle/input/dataset-for-lab2/VinaFood21/test")
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor()
+    ])
+
+    train_dataset = VinaFood("/kaggle/input/dataset-for-lab2/VinaFood21/train", transform = transform)
+    test_dataset  = VinaFood("/kaggle/input/dataset-for-lab2/VinaFood21/test", transform = transform)
 
     train_dataloader = DataLoader(
         dataset=train_dataset,
@@ -81,9 +107,26 @@ if __name__ == "__main__":
         current_score = scores[best_score_name]
         if current_score > best_score:
             best_score = current_score
+
+    print("\n---- Final Evaluation ----")
+    all_preds, all_labels = [], []
+    model.eval()
+
+    with torch.no_grad():
+        for items in test_dataloader:
+            images = items["image"].to("cuda")
+            labels = items["label"].to("cuda")
+            outputs = model(images)
+            preds = torch.argmax(outputs, dim=-1)
+            all_preds.extend(preds.tolist())
+            all_labels.extend(labels.tolist())
+
+    evaluate_per_class(all_preds, all_labels, num_classes=21)
+
     
 
 
        
+
 
 
