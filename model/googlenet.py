0efaacb9c16a190cf5d0523eb84ccd9bb973abb3
dikey4405 <167569079+dikey4405@users.noreply.github.com>
2025-10-30 21:52:31 +0700

    Update googlenet.py

--- model/googlenet.py
+++ model/googlenet.py
@@ -3,9 +3,9 @@ from torch import nn
 import torch.nn.functional as F
 
 class BaseConvBlock(nn.Module):
-    def __init__(self, in_chanels, out_chanels, **kwargs):
+    def __init__(self, in_channels, out_channels, **kwargs):
         super().__init__()
-        self.conv = nn.Conv2d(in_chanels, out_chanels, **kwargs)
+        self.conv = nn.Conv2d(in_channels, out_channels, **kwargs)
         self.relu = nn.ReLU(inplace=True)
 
     def forward(self, x):
@@ -13,29 +13,30 @@ class BaseConvBlock(nn.Module):
         x = self.relu(x)
         return x
         
+
 class Inception_Block(nn.Module):
     def __init__(self, in_channels, ch1x1, ch3x3reduce, ch3x3, ch5x5reduce, ch5x5, pool_proj):
         super().__init__()
 
         # 1x1 conv branch
-        self.branch1 = nn.BaseConvBlock(in_channels, ch1x1, kernel_size=1)
+        self.branch1 = BaseConvBlock(in_channels, ch1x1, kernel_size=1)
 
         # 1x1 conv -> 3x3 conv branch
         self.branch2 = nn.Sequential(
-            self.branch2_1 = nn.BaseConvBlock(in_channels, ch3x3reduce, kernel_size=1)
-            self.branch2_2 = nn.BaseConvBlock(ch3x3reduce, ch3x3, kernel_size=3, padding=1)
+            BaseConvBlock(in_channels, ch3x3reduce, kernel_size=1),
+            BaseConvBlock(ch3x3reduce, ch3x3, kernel_size=3, padding=1)
         )
 
         # 1x1 conv -> 5x5 conv branch
         self.branch3 = nn.Sequential(
-            self.branch3_1 = nn.Conv2d(in_channels, ch5x5reduce, kernel_size=1)
-            self.branch3_2 = nn.Conv2d(ch5x5reduce, ch5x5, kernel_size=5, padding=2)
+            BaseConvBlock(in_channels, ch5x5reduce, kernel_size=1),
+            BaseConvBlock(ch5x5reduce, ch5x5, kernel_size=5, padding=2)
         )
 
         # 3x3 maxpool -> 1x1 conv branch
         self.branch4 = nn.Sequential(
-            self.branch4_1 = nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
-            self.branch4_2 = nn.Conv2d(in_channels, pool_proj, kernel_size=1)
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1),
+            BaseConvBlock(in_channels, pool_proj, kernel_size=1)
         )
 
     def forward(self, features: torch.Tensor):
@@ -46,76 +47,77 @@ class Inception_Block(nn.Module):
         outputs = [branch1, branch2, branch3, branch4]
         return torch.cat(outputs, 1)
 
+
 class GoogLeNet(nn.Module):
-    def __init__(self):
+    def __init__(self, labels=21):
         super().__init__()
 
         # First convolutional layer
-        self.conv_1 = nn.BaseConvBlock(
-            in_channels = 3,
-            out_channels = 64,
-            kernel_size = 7
-            stride = 2
-            padding = 3
+        self.conv_1 = BaseConvBlock(
+            in_channels=3,
+            out_channels=64,
+            kernel_size=7,
+            stride=2,
+            padding=3
         )
 
         self.pooling_1 = nn.MaxPool2d(
-            kernel_size = 3,
-            stride = 2,
-            ceil_mode = True
+            kernel_size=3,
+            stride=2,
+            ceil_mode=True
         )
         
         # Second convolutional layer
-        self.conv_2_1 = nn.BaseConvBlock(
-            in_channels = 64,
-            out_channels = 64,
-            kernel_size = 1
+        self.conv_2_1 = BaseConvBlock(
+            in_channels=64,
+            out_channels=64,
+            kernel_size=1
         )
         
-        self.conv_2_2 = nn.Conv2d(
-            in_channels = 64,
-            out_channels = 192,
-            kernel_size = 3,
-            padding = 1
+        self.conv_2_2 = BaseConvBlock(
+            in_channels=64,
+            out_channels=192,
+            kernel_size=3,
+            padding=1
         )
 
         self.pooling_2 = nn.MaxPool2d(
-            kernel_size = 3,
-            padding = 2,
-            ceil_mode = True
+            kernel_size=3,
+            stride=2,
+            ceil_mode=True
         )
 
         # Third convolutional layer
-        self.inteption_3a = Inception_Block(192,64,96,128,16,32,32)
-        self.inteption_3b = Inception_Block(256,128,128,192,32,96,64)
+        self.inception_3a = Inception_Block(192, 64, 96, 128, 16, 32, 32)
+        self.inception_3b = Inception_Block(256, 128, 128, 192, 32, 96, 64)
 
         self.pooling_3 = nn.MaxPool2d(
-            kernel_size = 3,
-            stride = 2,
-            ceil_mode = True
+            kernel_size=3,
+            stride=2,
+            ceil_mode=True
         )
 
         # Fourth convolutional layer
-        self.inteption_4a = Inception_Block(480, 192, 96, 208, 16, 48, 64)
-        self.inteption_4b = Inception_Block(512, 160, 112, 224, 24, 64, 64)
+        self.inception_4a = Inception_Block(480, 192, 96, 208, 16, 48, 64)
+        self.inception_4b = Inception_Block(512, 160, 112, 224, 24, 64, 64)
         self.inception_4c = Inception_Block(512, 128, 128, 256, 24, 64, 64)
         self.inception_4d = Inception_Block(512, 112, 144, 288, 32, 64, 64)
         self.inception_4e = Inception_Block(528, 256, 160, 320, 32, 128, 128)
 
         self.pooling_4 = nn.MaxPool2d(
-            kernel_size = 3,
-            stride = 2,
-            ceil_mode = True
+            kernel_size=3,
+            stride=2,
+            ceil_mode=True
         )
 
         # Fifth convolutional layer
         self.inception_5a = Inception_Block(832, 256, 160, 320, 32, 128, 128)
         self.inception_5b = Inception_Block(832, 384, 192, 384, 48, 128, 128)
 
-        self.pooling_5 = nn.AdaptiveAvgPool2d((1,1))
+        self.pooling_5 = nn.AdaptiveAvgPool2d((1, 1))
 
         self.dropout = nn.Dropout(p=0.4)
-        self.linear = nn.Linear(in_features=1024, out_features=21)
+        self.linear = nn.Linear(in_features=1024, out_features=labels)
 
         self._initialize_weights()
 
@@ -130,7 +132,6 @@ class GoogLeNet(nn.Module):
                 nn.init.constant_(m.bias, 0)
 
     def forward(self, feature: torch.Tensor):
-
         x = self.conv_1(feature)
         x = self.pooling_1(x)
 
@@ -138,12 +139,12 @@ class GoogLeNet(nn.Module):
         x = self.conv_2_2(x)
         x = self.pooling_2(x)
 
-        x = self.inteption_3a(x)
-        x = self.inteption_3b(x)
+        x = self.inception_3a(x)
+        x = self.inception_3b(x)
         x = self.pooling_3(x)
 
-        x = self.inteption_4a(x)
-        x = self.inteption_4b(x)
+        x = self.inception_4a(x)
+        x = self.inception_4b(x)
         x = self.inception_4c(x)
         x = self.inception_4d(x)
         x = self.inception_4e(x)
@@ -158,4 +159,3 @@ class GoogLeNet(nn.Module):
         x = self.linear(x)
 
         return x
-
